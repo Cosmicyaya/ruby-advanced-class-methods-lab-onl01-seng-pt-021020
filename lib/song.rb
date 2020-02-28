42ec87448d495cd1c244539efcc5c068c3ce336e
Ryan Hollins <ballbaroi@gmail.com>
2020-02-28 16:52:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,32 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+    
+    def self.create
+      song = self.new
+      self.all << song
+      song
+  end
+  def save
+    self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
 end
